24e23234ee2f94fc3d7420f3a54ac8c10f5b06bb
REYNALDO CASILLA <58133785+casillarecker@users.noreply.github.com>
2021-02-28 21:55:48 -0400

    Update Variables_6.java

--- src/Variables_6.java
+++ src/Variables_6.java
@@ -18,6 +18,10 @@ public class Variables_6 {
 	System.out.println(n);
 	
 	//Reynaldo te quiero porque eres valiente y fuerte, no te rendiras jamas siempre seguiras a Dios
+		
+	int m = 50 , int m1;
+	int t = m+m1;
+	System.out.println(n);
 	
 	}
 
